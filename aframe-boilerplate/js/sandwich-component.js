5d799d15d39fcc085894aa0163aef8087993e491
nelmarianna <aniela.opolski@gmail.com>
2017-03-04 20:43:40 -0500

    make image appear inside a-entity

--- aframe-boilerplate/js/sandwich-component.js
+++ aframe-boilerplate/js/sandwich-component.js
@@ -1,10 +1,25 @@
-AFRAME.registerComponent('evil-sandwich' 
+AFRAME.registerComponent('evil-sandwich',{
+	init: function() {
+		var test = getNewImage();
+		test.setAttribute('src','img/sandwich.png');
+		test.setAttribute('width','0.7');
+		test.setAttribute('height','0.7');
+		test.setAttribute('rotation', "20 90 20");
+		test.setAttribute('position',"2 2 1");
+	}
 
-	schema: {},
-  init: function () {},
-  update: function () {},
-  tick: function () {},
-  remove: function () {},
-  pause: function () {},
-  play: function () {}
-});
+
+}
+);
+
+function getNewImage() {
+  var el = document.createElement('a-image');
+  document.querySelector('a-scene').appendChild(el);
+  return el
+}
+
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
