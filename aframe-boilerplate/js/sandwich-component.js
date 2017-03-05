e92593487bc719f88098804478b25d0896c9b4e6
nelmarianna <aniela.opolski@gmail.com>
2017-03-05 04:02:37 -0500

    sandwiches movegit status

--- aframe-boilerplate/js/sandwich-component.js
+++ aframe-boilerplate/js/sandwich-component.js
@@ -1,5 +1,4 @@
 var sandwichHolder = [];
-var sandywichHolder = [];
 
 for(i=0; i<8; i++){
 		var xco=getRandomInt(-4,4);
@@ -15,26 +14,33 @@ for(i=0; i<8; i++){
 		zco=getRandomInt(-4,4);
 	}
 	generateImage(xco, yco, zco, i);
-	addAnimationToImage();
 }
 
 function generateImage(x, y, z, num){
 	AFRAME.registerComponent('evil-sandwich-'+num,{
 		init: function() {
 			var test = getNewImage();
+			var newId= 'evil-sandwich-'+num;
+			test.setAttribute('id',newId);
 			test.setAttribute('src','img/sandwich.png');
 			test.setAttribute('width','0.7');
 			test.setAttribute('height','0.7');
-			// test.setAttribute('rotation', "20 90 20");
+			test.setAttribute('rotation', "0 0 0");
 			test.setAttribute('position', x+" "+y+" "+z);
 			test.setAttribute('visible','false');
+			test.setAttribute('spin-me','target', '#'+newId);
 			sandwichHolder.push(test);
- 		}
-
+		}
 	});
 }
 
 
+// Set multiple attributes at once (since no jQuery)
+function setAttributes(el, attrs) {
+  for(var key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
 
 function getNewImage() {
   var el = document.createElement('a-image');
@@ -42,11 +48,14 @@ function getNewImage() {
   return el
 }
 
-
 function getRandomInt(min, max) {
-
     return Math.floor(Math.random() * (max - min)) + min;
 }
+
+function appear(){
+	var sandy = sandwichHolder.pop();
+	sandy.setAttribute('visible','true');
+	}
 
 setTimeout(appear, 10000)
 setTimeout(appear, 15000)
@@ -56,26 +65,23 @@ setTimeout(appear, 30000)
 setTimeout(appear, 35000)
 setTimeout(appear, 40000)
 
-function appear(){
-	console.log("heyy")
-	var sandy=sandwichHolder.pop();
-	sandy.setAttribute('visible','true');
-	}
-	
-function getNewAnimation(parentEl) {
-  var el = document.createElement('a-animation');
-  parentEl.appendChild(el);
-  return el
-}
 
-function addAnimationToImage(){
-	for(i=0;i<8;i++){
-	var test = sandwichHolder[i];
-	var newTest=getNewAnimation(test);
-	newTest.setAttribute('attribute','rotation');
-	newTest.setAttribute('repeat','indefinite');
-	newTest.setAttribute('to','0 360 0');
-		
-	
-	}
-}
+
+//ANIMATION
+AFRAME.registerComponent('spin-me', {
+  schema: {
+    target : { type : "selector"}
+  },
+
+  init: function() {
+    var spinningAnimation = document.createElement('a-animation');
+
+    setAttributes(spinningAnimation, {
+      attribute: "rotation",
+      repeat:    "indefinite",
+      to:        "0 360 0",
+    })
+
+    this.data.target.appendChild(spinningAnimation)
+  }
+})
