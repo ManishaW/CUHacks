83b4b6a1eaf35077520d2947d11b30f2a853d060
nelmarianna <aniela.opolski@gmail.com>
2017-03-04 19:39:05 -0500

    new component

---
+++ aframe-boilerplate/js/sandwich-component.js
@@ -0,0 +1,10 @@
+AFRAME.registerComponent('evil-sandwich' 
+
+	schema: {},
+  init: function () {},
+  update: function () {},
+  tick: function () {},
+  remove: function () {},
+  pause: function () {},
+  play: function () {}
+});
