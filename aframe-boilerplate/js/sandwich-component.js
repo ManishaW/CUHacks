c7daac3e8429166e9a20e20fe27e6a79882cead8
nelmarianna <aniela.opolski@gmail.com>
2017-03-04 22:30:05 -0500

    component pic :)

--- aframe-boilerplate/js/sandwich-component.js
+++ aframe-boilerplate/js/sandwich-component.js
@@ -1,16 +1,20 @@
-AFRAME.registerComponent('evil-sandwich',{
-	init: function() {
-		var test = getNewImage();
-		test.setAttribute('src','img/sandwich.png');
-		test.setAttribute('width','0.7');
-		test.setAttribute('height','0.7');
-		test.setAttribute('rotation', "20 90 20");
-		test.setAttribute('position',"2 2 1");
-	}
-
+function miniGameStart(){
+generateImage();
 
 }
-);
+
+function generateImage(){
+	AFRAME.registerComponent('evil-sandwich',{
+		init: function() {
+			var test = getNewImage();
+			test.setAttribute('src','img/sandwich.png');
+			test.setAttribute('width','0.7');
+			test.setAttribute('height','0.7');
+			test.setAttribute('rotation', "20 90 20");
+			test.setAttribute('position',"2 2 1");
+		}
+	});
+}
 
 function getNewImage() {
   var el = document.createElement('a-image');
@@ -23,3 +27,4 @@ function setAttributes(el, attrs) {
     el.setAttribute(key, attrs[key]);
   }
 }
+
